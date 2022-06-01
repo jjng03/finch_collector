979424eb23e8917b8f42d2ed46285bcae869968c
James Jung <jjng930@gmail.com>
2022-06-01 08:34:24 -0500

    created static folder for CSS and Js files

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Pokemon Home")
+class Home(TemplateView):
+    template_name = "home.html"
