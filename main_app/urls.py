ffa31c7a5d32616609dfcf5b274bf82047bfed97
James Jung <jjng930@gmail.com>
2022-06-01 12:54:36 -0500

    created a delete route

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('pokedex/', views.PokemonList.as_view(), name="pokemon_list"),
     path('pokedex/new/', views.PokemonCreate.as_view(), name="pokemon_create"),
-    path('pokedex/<int:pk>/', views.PokemonDetail.as_view(), name="pokemon_detail")
+    path('pokedex/<int:pk>/', views.PokemonDetail.as_view(), name="pokemon_detail"),
+    path('pokedex/<int:pk>/update', views.PokemonUpdate.as_view(), name="pokemon_update"),
+    path('pokedex/<int:pk>/delete', views.PokemonDelete.as_view(), name="pokemon_delete"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from .models import Pokemon
+from django.urls import reverse
 
 class Home(TemplateView):
     template_name = "home.html"
@@ -28,8 +29,21 @@ class PokemonCreate(CreateView):
     model = Pokemon
     fields = ['name', 'img', 'bio', 'verified_pokemon']
     template_name = "pokemon_create.html"
-    success_url = "/pokedex/"
+    def get_success_url(self):
+        return reverse('pokemon_detail', kwargs={'pk: self.object.pk'})
 
 class PokemonDetail(DetailView):
     model = Pokemon
     template_name = "pokemon_detail.html"
+
+class PokemonUpdate(UpdateView):
+    model = Pokemon
+    fields = ['name', 'img', 'bio', 'verified_pokemon']
+    template_name = "pokemon_update.html"
+    def get_success_url(self):
+        return reverse('pokemon_detail', kwargs={'pk': self.object.pk})
+
+class PokemonDelete(DeleteView):
+    model = Pokemon
+    template_name = "pokemon_delete_confirmation.html"
+    success_url = "/pokedex/"
