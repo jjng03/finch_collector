834649af2dbb124652042f3a9924ed8f33dcea9b
James Jung <jjng930@gmail.com>
2022-06-01 10:19:05 -0500

    added data to main_app_pokemon database and imported model to class PokemonList

--- main_app/views.py
+++ main_app/views.py
@@ -2,43 +2,16 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Pokemon
 
 class Home(TemplateView):
     template_name = "home.html"
 
 
-
-class Pokemon:
-    def __init__(self, name, image, bio):
-        self.name = name
-        self.image = image
-        self.bio = bio
-
-
-pokedex = [
-  Pokemon("Charmander",
-          "https://www.nicepng.com/png/detail/278-2789894_pokemon-charmander-vector.png",
-          "A cute fire pokemon."),
-  Pokemon("Pikachu",
-          "https://www.nicepng.com/png/detail/485-4856027_pikachu-pikachu-ears-cute-pikachu-cute-pokemon-pokemon.png",
-          "A yellow rat."),
-  Pokemon("Lugia",
-          "https://www.nicepng.com/png/detail/225-2258279_pokmon-lugia-or-latios-lugia-pokemon.png",
-          "A white bird."),
-  Pokemon("Mew",
-          "https://www.nicepng.com/png/detail/62-622852_ignore-words-mew-png.png",
-          "An alien."),
-  Pokemon("Magikarp",
-          "https://www.nicepng.com/png/detail/149-1492459_magikarp-pokemon-go-magikarp.png",
-          "Sushi."),
-  Pokemon("Hitmonlee",
-          "https://www.kindpng.com/picc/m/276-2765609_pokemon-hitmonlee-png-transparent-png.png",
-          "Bruce Lee as a Pokemon."),
-]
 class PokemonList(TemplateView):
     template_name = "pokemon_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["pokedex"] = pokedex
+        context["pokemon"] = Pokemon.objects.all()
         return context
